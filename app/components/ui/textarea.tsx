5cdb40d32c0512d9e84432f6ea4b6df57e3ab9fd
Rodrigo Isaac Carrera Estrada <rolodef1@gmail.com>
2026-02-09 17:22:26 -0500

    Delete app directory

--- app/components/ui/textarea.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Textarea = React.forwardRef<HTMLTextAreaElement, React.TextareaHTMLAttributes<HTMLTextAreaElement>>((props, ref) => {
-  return <textarea ref={ref} className="w-full rounded-md border border-slate-200 px-3 py-2 focus:outline-none focus:ring-2 focus:ring-pink-200" {...props} />;
-});
-
-Textarea.displayName = "Textarea";
-
-export default Textarea;
